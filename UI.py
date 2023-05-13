9c7005e566b5e234ad629b58339f252b26f8aeaa
SHemarati <hasan.emarati.12652@gmail.com>
2023-05-13 14:01:37 +0330

    select Item in check box

--- UI.py
+++ UI.py
@@ -34,28 +34,28 @@ class DiskUI:
         self.refresh_button.pack(side=tk.TOP, padx=5, pady=10)
 
         # Only Read button
-        self.read_button = ttk.Button(self.button_frame, text="Only Read", command=self.Read_Only)
+        self.read_button = ttk.Button(self.button_frame, text="Only Read", command=lambda: self.perform_action("Read_Only"))
         self.read_button.pack(side=tk.TOP, padx=5, pady=10)
 
         # Read/Write button
-        self.read_write_button = ttk.Button(self.button_frame, text="Read/Write", command=self.Read_Write)
+        self.read_write_button = ttk.Button(self.button_frame, text="Read/Write", command=lambda: self.perform_action("Read_Write"))
         self.read_write_button.pack(side=tk.TOP, padx=5, pady=10)
 
         # Format Disk button
-        self.format_button = ttk.Button(self.button_frame, text="Format", command=self.Format_Disk)
+        self.format_button = ttk.Button(self.button_frame, text="Format", command=lambda: self.perform_action("Format_Disk"))
         self.format_button.pack(side=tk.TOP, padx=5, pady=10)
 
         # Eject Disk button
-        self.eject_button = ttk.Button(self.button_frame, text="Eject Disk", command=self.Eject_Disk)
+        self.eject_button = ttk.Button(self.button_frame, text="Eject Disk", command=lambda: self.perform_action("Eject_Disk"))
         self.eject_button.pack(side=tk.TOP, padx=5, pady=10)
-
-        # Exit button
-        self.exit_button = ttk.Button(self.button_frame, text="Exit", command=self.root.destroy)
-        self.exit_button.pack(side=tk.BOTTOM, padx=5, pady=10)
 
         # Reset button
         self.reset_button = ttk.Button(self.button_frame, text="Reset", command=self.reset)
         self.reset_button.pack(side=tk.BOTTOM, padx=5, pady=10)
+
+        # Exit button
+        self.exit_button = ttk.Button(self.button_frame, text="Exit", command=self.root.destroy)
+        self.exit_button.pack(side=tk.BOTTOM, padx=5, pady=10)
 
         self.root.mainloop()
 
@@ -72,7 +72,7 @@ class DiskUI:
 
     def Eject_Disk(self):
         self.disk_info.Eject_Disk()
-    
+
     def Format_Disk(self):
         self.disk_info.Format_Disk()
 
@@ -82,9 +82,25 @@ class DiskUI:
     def Read_Write(self):
         self.disk_info.Read_Write()
 
+    def perform_action(self, action):
+        # Print selected items
+        selected_items = [checkbox.cget("text") for checkbox in self.disk_checkboxes if checkbox.instate(['selected'])]
+        print("Selected items:", selected_items)
+
+        # Perform action on selected items
+        if action == "Eject_Disk":
+            self.Eject_Disk()
+        elif action == "Format_Disk":
+            self.Format_Disk()
+        elif action == "Read_Only":
+            self.Read_Only()
+        elif action == "Read_Write":
+            self.Read_Write()
+
     def reset(self):
         self.root.destroy()
         DiskUI()
+
 
 if __name__ == '__main__':
     DiskUI()
--- USB_test.py
+++ USB_test.py
@@ -1,30 +1,111 @@
+import tkinter as tk
+from tkinter import ttk
+from ttkthemes import ThemedStyle
+import wmi
 
+class Disk:
+    def refresh_disk_info(self):
+        c = wmi.WMI()
+        disk_info = []
+        for disk in c.Win32_LogicalDisk():
+            free_space = round(int(disk.FreeSpace)/(1024**3), 2) if disk.FreeSpace is not None else "-"
+            size = round(int(disk.Size)/(1024**3), 2) if disk.Size is not None else "-"
+            disk_info += [f'{disk.DeviceID}{disk.VolumeName} Free Space: {free_space} GB Size: {size} GB File System: {disk.FileSystem} VolumeDirty: {disk.VolumeDirty} Serial Number: {disk.VolumeSerialNumber}']
+        return disk_info
+    
+    def Eject_Disk(self):
+        print("Disk is Ejecting")
+        
+    def Format_Disk(self):
+        print("Formatting disk")
+        
+    def Read_Only(self):
+        print("Read-Only")
+        
+    def Read_Write(self):
+        print("Read/Write")
+        
+    def perform_action(self, action, selected_items):
+        # Print selected items
+        print("Selected items:", selected_items)
 
-# import psutil
-# import wmi
+        # Perform action on selected items
+        if action == "Eject_Disk":
+            self.Eject_Disk()
+        elif action == "Format_Disk":
+            self.Format_Disk()
+        elif action == "Read_Only":
+            self.Read_Only()
+        elif action == "Read_Write":
+            self.Read_Write()
 
+class DiskUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Disk")
+        self.style = ThemedStyle(self.root)
+        self.style.set_theme("equilux")
+        self.frame = ttk.Frame(self.root, padding=10)
+        self.frame.pack(fill=tk.BOTH, expand=True)
+        self.root.minsize(400, 200)
+        self.root.maxsize(800, 500)
 
+        # Create an instance of Disk class
+        self.disk_info = Disk()
 
+        # Disk info checkboxes
+        self.disk_checkboxes_frame = ttk.Frame(self.frame)
+        self.disk_checkboxes_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.disk_checkboxes = []
+        self.refresh()
 
-# partitions = psutil.disk_partitions()
-# for partition in partitions:
-#     if "removable" in partition.opts:
-#         print(f"{partition.device} is a removable drive")
-#         try:
-#             usage = psutil.disk_usage(partition.mountpoint)
-#             print(f"Total space: {round(usage.total / (1024**3), 2)} GB, "
-#                   f"Free space: {round(usage.free / (1024**3), 2)} GB, "
-#                   f"Used space: {round(usage.used / (1024**3), 2)} GB")
-#             read_write_info = psutil.disk_io_counters(perdisk=True).get(partition.device)
-#             if read_write_info:
-#                 print(f"Read count: {read_write_info.read_count}, "
-#                       f"Write count: {read_write_info.write_count}, "
-#                       f"Read bytes: {round(read_write_info.read_bytes / (1024**2), 2)} MB, "
-#                       f"Write bytes: {round(read_write_info.write_bytes / (1024**2), 2)} MB")
-#             else:
-#                 print("No read/write info available")
-#         except PermissionError:
-#             print(f"No access permission to {partition.device}")
-#         except FileNotFoundError:
-#             print(f"{partition.device} does not exist")
-#         print("------------------------") 
+        # Button frame
+        self.button_frame = ttk.Frame(self.frame)
+        self.button_frame.pack(side=tk.LEFT)
+
+        # Refresh button
+        self.refresh_button = ttk.Button(self.button_frame, text="Refresh", command=self.refresh)
+        self.refresh_button.pack(side=tk.TOP, padx=5, pady=10)
+
+        # Only Read button
+        self.read_button = ttk.Button(self.button_frame, text="Only Read", command=lambda: self.perform_action("Read_Only", self.get_selected_items()))
+        self.read_button.pack(side=tk.TOP, padx=5, pady=10)
+
+        # Read/Write button
+        self.read_write_button = ttk.Button(self.button_frame, text="Read/Write", command=lambda: self.perform_action("Read_Write", self.get_selected_items()))
+        self.read_write_button.pack(side=tk.TOP, padx=5, pady=10)
+
+        # Eject button
+        self.eject_button = ttk.Button(self.button_frame, text="Eject", command=lambda: self.perform_action("Eject_Disk", self.get_selected_items()))
+        self.eject_button.pack(side=tk.TOP, padx=5, pady=10)
+
+        # Format button
+        self.format_button = ttk.Button(self.button_frame, text="Format", command=lambda: self.perform_action("Format_Disk", self.get_selected_items()))
+        self.format_button.pack(side=tk.TOP, padx=5, pady=10)
+
+        self.root.mainloop()
+
+    def refresh(self):
+        # Clear checkboxes
+        for checkbox in self.disk_checkboxes:
+            checkbox.destroy()
+        self.disk_checkboxes = []
+
+        # Get disk info and create checkboxes
+        disk_info = self.disk_info.refresh_disk_info()
+        for item in disk_info:
+            checkbox = ttk.Checkbutton(self.disk_checkboxes_frame, text=item)
+            checkbox.pack(side=tk.TOP, anchor=tk.W)
+            self.disk_checkboxes.append(checkbox)
+
+    def get_selected_items(self):
+        selected_items = []
+        for checkbox in self.disk_checkboxes:
+            if checkbox.instate(['selected']):
+                selected_items.append(checkbox['text'])
+        return selected_items
+
+    def perform_action(self, action, selected_items):
+        self.disk_info.perform_action(action, selected_items)
+
+DiskUI()
